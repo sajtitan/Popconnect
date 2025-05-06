fab781fedc3616b7ab4b793e0edf30e01e24910c
sajtitan <sankalprout2004@gmail.com>
2025-05-06 15:28:23 +0530

    Delete bf test.py

--- bf test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://localhost:5001/analyze"
-data = {"request_data": "test"}
-headers = {"Content-Type": "application/json"}
-
-for i in range(15):  # Send 15 requests
-    response = requests.post(url, json=data, headers=headers)
-    print(f"Request {i+1}: {response.status_code} - {response.text}")
